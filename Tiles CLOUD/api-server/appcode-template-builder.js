dbe605a2a63b178c117b705c25e36143f0072387
sivapalan <varuns@stud.ntnu.no>
2016-04-29 10:41:49 +0200

    Added support for group in template builder

--- Tiles CLOUD/api-server/appcode-template-builder.js
+++ Tiles CLOUD/api-server/appcode-template-builder.js
@@ -1,26 +1,28 @@
 exports.build = function(userId, templateSections) {
     var code = '';
     if (templateSections.connectToServer) {
-        code += 'var client = new TilesClient(\'' + userId + '\').connect();';
+        code += 'var client = new TilesClient(\'' + userId + '\'';
 
         if (templateSections.setTargetGroup) {
-            // TODO: Implement 'Target Group' feature
+            code += ', appConfig.group';
         }
 
+        code += ').connect();';
+
         if (templateSections.evtConnectedToServer) {
-            code += '\r\n\r\nclient.on(\'connect\', function(){\r\n    \r\n});';
+            code += '\r\n\r\nclient.on(\'connect\', function() {\r\n    \r\n});';
         }
 
         if (templateSections.evtMsgReceived) {
-            code += '\r\n\r\nclient.on(\'receive\', function(tileId, event){\r\n    \r\n});'
+            code += '\r\n\r\nclient.on(\'receive\', function(tileId, event) {\r\n    \r\n});'
         }
 
         if (templateSections.evtDeviceConnected) {
-            code += '\r\n\r\nclient.on(\'tileRegistered\', function(tileId){\r\n    \r\n});'
+            code += '\r\n\r\nclient.on(\'tileRegistered\', function(tileId) {\r\n    \r\n});'
         }
 
         if (templateSections.evtDeviceDisconnected) {
-            code += '\r\n\r\nclient.on(\'tileUnregistered\', function(tileId){\r\n    \r\n});';
+            code += '\r\n\r\nclient.on(\'tileUnregistered\', function(tileId) {\r\n    \r\n});';
         }
     }
     return code;
