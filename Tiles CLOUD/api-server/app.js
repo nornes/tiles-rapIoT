b9530481809d400cdd30323ea036f1a47a1ff79f
sivapalan <varuns@stud.ntnu.no>
2016-05-01 12:46:22 +0200

    Added middleware for setting CORS headers

--- Tiles CLOUD/api-server/app.js
+++ Tiles CLOUD/api-server/app.js
@@ -30,6 +30,21 @@ app.set('buildDate', new Date().toUTCString());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Set CORS headers
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // Intercept OPTIONS requests
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  }
+);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
