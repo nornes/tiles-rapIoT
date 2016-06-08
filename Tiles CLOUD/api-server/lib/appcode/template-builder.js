d0248f745d7df7dd438144d332c478f8cbdc2b69
sivapalan <varuns@stud.ntnu.no>
2016-06-08 10:58:11 +0200

    Added comments to code template

--- Tiles CLOUD/api-server/lib/appcode/template-builder.js
+++ Tiles CLOUD/api-server/lib/appcode/template-builder.js
@@ -10,19 +10,19 @@ exports.build = function(userId, templateSections) {
         code += ', \'localhost\').connect();';
 
         if (templateSections.evtConnectedToServer) {
-            code += '\r\n\r\nclient.on(\'connect\', function() {\r\n    \r\n});';
+            code += '\r\n\r\nclient.on(\'connect\', function() {\r\n    // App is connected to server\r\n});';
         }
 
         if (templateSections.evtMsgReceived) {
-            code += '\r\n\r\nclient.on(\'receive\', function(tileId, event) {\r\n    \r\n});'
+            code += '\r\n\r\nclient.on(\'receive\', function(tileId, event) {\r\n    // An event is received from a Tile\r\n});'
         }
 
         if (templateSections.evtDeviceConnected) {
-            code += '\r\n\r\nclient.on(\'tileRegistered\', function(tileId) {\r\n    \r\n});'
+            code += '\r\n\r\nclient.on(\'tileRegistered\', function(tileId) {\r\n    // A Tile is registered (connected)\r\n});'
         }
 
         if (templateSections.evtDeviceDisconnected) {
-            code += '\r\n\r\nclient.on(\'tileUnregistered\', function(tileId) {\r\n    \r\n});';
+            code += '\r\n\r\nclient.on(\'tileUnregistered\', function(tileId) {\r\n    // A Tile is unregistered (disconnected)\r\n});';
         }
     }
     return code;
